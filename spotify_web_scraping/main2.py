2de32a43be979a647c61c41e10faa55aabf04894
S RUUSHEKA AKILAVARSHINI <ruushekas@gmail.com>
2025-03-03 21:25:18 +0530

    Add files via upload

---
+++ spotify_web_scraping/main2.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Scraping Billboard 100
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+billboard_url = "https://www.billboard.com/charts/hot-100/" + date
+response = requests.get(url=billboard_url, headers=header)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
+
+# Spotify Authentication
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id="938bbb865df44744bf784b5b3a4f4bc5",
+        client_secret="86c78dd4a8614257b218f9eec1c42c1e",
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
+
+# Searching Spotify for songs by title
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    try:
+        result = sp.search(q=f"track:{song} year:{year}", type="track", limit=1)
+        tracks = result.get("tracks", {}).get("items", [])
+        
+        if tracks:  # Check if the list is not empty
+            uri = tracks[0]["uri"]
+            song_uris.append(uri)
+            print(f"✅ Found: {song}")
+        else:
+            print(f"❌ {song} not found on Spotify. Skipped.")
+
+    except Exception as e:
+            print(f"⚠️ Error searching for {song}: {e}")
+
+# Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+# print(playlist)
+
+# Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
